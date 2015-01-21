c561a5ee0c23a5854cb8bbbe71850efb0d8e0fcf
KBondanza <kelly.bondanza@gmail.com>
2015-01-21 04:40:51 -0500

    started to add tracks and individual prices in the track spec. wrote the first test for album spec, which specifys 10 tracks, with the individual prices but when added together come up with a total price for the album.

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -3,3 +3,40 @@ require 'pry'
 require 'byebug'
 
 require_relative '../lib/album'
+
+RSpec.describe Album do
+  it 'it contains a collection of tracks and the total price for the album' do
+    album = Album.new
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    album.add_entry(Track.new(:name => Name.new(
+      :price => Money.new(0.99, :USD)
+    )))
+    expect(order.total).to eq(Money.new(9.90, :USD))
+  end
+end
--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -3,3 +3,16 @@ require 'pry'
 require 'byebug'
 
 require_relative '../lib/track'
+
+RSpec.describe Track do
+  it 'it lists all the tracks and their individual prices, by respected artist' do
+    track = Track.new
+    track.add_entry(Artist.new(:name => Name.new(
+      :price => Money.new(1.11, :USD)
+    )))
+    track.add_entry(Artist.new(:name => Name.new(
+      :price => Money.new(2.22, :USD)
+    )))
+    expect(order.total).to eq(Money.new(5.55, :USD))
+  end
+end
