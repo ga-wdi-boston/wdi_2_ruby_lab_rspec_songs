1b60b8f65158029075883f795915f13134aa3be2
KBondanza <kelly.bondanza@gmail.com>
2015-01-21 07:47:17 -0500

    added in comments for the instructions.

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -5,6 +5,12 @@ require 'byebug'
 require_relative '../lib/album'
 require_relative '../lib/track'
 
+# Has a title and a list of Track objects (order matters)
+# Has a duration (combined duration of all tracks)
+# Has a price (combined price of all tracks)
+# Has a list of artists (combined artists of all tracks in alphabetical order – no duplicates)
+# Can determine whether the album is a single (has exactly one track)
+
 RSpec.describe Album do
   it 'it contains a collection of tracks and the total price for the album' do
     album = Album.new
