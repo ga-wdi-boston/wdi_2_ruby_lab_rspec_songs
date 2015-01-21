486dfd408ec84260c2c74d25c137b29a7d5eb604
J.Marx <jennyrutledge@gmail.com>
2015-01-21 08:44:48 -0500

    Update comments in album and track classes.

--- lib/track.rb
+++ lib/track.rb
@@ -7,13 +7,13 @@ class Track
     @duration = info[:duration]
     @price = info[:price]
   end
-
+  #Sets a discounted price for a track.
   def set_discount (percent)
    raise ArgumentError, 'Argument cannot exceed 100%' unless percent < 1.0
 
      @price = (@price - @price * percent).round(2)
   end
-
+  #Restores a prices for a track, given it's originial discount.
   def restore_value(percent)
   raise ArgumentError, 'Argument cannot exceed 100%' unless percent < 1.0
 
