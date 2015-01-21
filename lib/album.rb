78092b1b65c239dcaa4e8cbf9010902eeed4e1a2
KBondanza <kelly.bondanza@gmail.com>
2015-01-21 05:25:29 -0500

    added tracks to track_spec.rb. Also added class def to album.rb

---
+++ lib/album.rb
@@ -0,0 +1,8 @@
+require 'byebug'
+require 'pry'
+
+class Album
+  def initialize(album)
+    @album = album
+  end
+end
--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -6,13 +6,15 @@ require_relative '../lib/track'
 
 RSpec.describe Track do
   it 'it lists all the tracks and their individual prices, by respected artist' do
-    track = Track.new
-    track.add_entry(Artist.new(:name => Name.new(
-      :price => Money.new(1.11, :USD)
-    )))
-    track.add_entry(Artist.new(:name => Name.new(
-      :price => Money.new(2.22, :USD)
-    )))
-    expect(order.total).to eq(Money.new(5.55, :USD))
+    track = Track.new('Invincible', ['Adelitas Way'],  3:10, '0.99')
+    track = Track.new('Scream', ['Adelitas Way'],  3:34, '0.99')
+    track = Track.new('So Cold', ['Breaking Benjamin'],  3:56, '0.99')
+    track = Track.new('Sooner or Later', ['Breaking Benjamin'],  3:24, '0.99')
+    track = Track.new('The Phoenix', ['Fall Out Boy'],  4:04, '0.99')
+    track = Track.new('Centuries', ['Fall Out Boy'],  3:51, '0.99')
+    track = Track.new('Alive and Kicking', ['NonPoint'],  3:59, '0.99')
+    track = Track.new('Broken Down', ['Sevendust'],  3:23, '0.99')
+    track = Track.new('Separate', ['Sevendust'],  3:41, '0.99')
+    track = Track.new('Karma', ['Sevendust'],  3:43, '0.99')
   end
 end
