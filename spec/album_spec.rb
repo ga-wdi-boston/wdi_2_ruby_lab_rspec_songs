229558eb7a6f1e0642cad784910c7f899de46e75
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-01-20 22:44:22 -0500

    create test to check for album creation

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -3,9 +3,41 @@ require 'pry'
 require 'byebug'
 
 require_relative '../lib/album'
+require_relative '../lib/track.rb'
 
-RSpec.describe album do
+RSpec.describe Album do
 
+  subject(:album){Album.new('1989')}
+
+  describe '#initialize' do
+    it 'creates a new instance of the Album class' do
+      expect(album).to be_a Album
+    end
+  end
+  describe '#list_of_tracks' do
+    it 'create a array to hold your tracks' do
+      my_album = Album.new([])
+      expect(my_album.list_of_tracks).to eq 12
+    end
+  end
 
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+# describe '#sum_of_numeric_elements' do
+#     it 'sums all Float and Fixnum elements' do
+#       expect(@array.sum_of_numeric_elements).to be 41.1
+#     end
+#   end
